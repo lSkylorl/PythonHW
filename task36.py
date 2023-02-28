24680c0870af9cd7253317603b60b5c4f2f73439
lSkylorl <116230779+lSkylorl@users.noreply.github.com>
2023-02-28 14:55:14 +0400

    Delete task36.py

--- task36.py
+++
@@ -1,9 +0,0 @@
-def print_operation_table(operation, num_rows=6, num_columns=6):
-    for i in range(1, num_rows + 1):
-        ans = []
-        for j in range(1, num_columns + 1):
-            ans.append(str(operation(i, j)))
-        print("\t".join(ans))
- 
- 
-print_operation_table(lambda x, y: x * y)
